00b0682fd895545f3a7a69bfb0cf754756367984
Thomas Eng <thomaseng@live.no>
2024-08-07 23:59:34 +0200

    Initial commit

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,19 @@
 extends CharacterBody2D
- 
+
+@onready var camera = $Camera2D
+var is_local_player = false
+
+func _ready():
+	if is_multiplayer_authority():
+		is_local_player = true
+		camera.make_current()
+
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
  
 func _physics_process(_delta):
 	if is_multiplayer_authority():
-		velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down") * 400
+		velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down") * 100
+		
 	move_and_slide()
  
