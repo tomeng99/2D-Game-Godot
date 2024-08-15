b80ba37ad1ba6a17f20c89fa3497d33d79058066
Thomas Eng <thomaseng@live.no>
2024-08-15 11:03:12 +0200

    Simple Grass and player added

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -14,6 +14,5 @@ func _enter_tree():
 func _physics_process(_delta):
 	if is_multiplayer_authority():
 		velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down") * 100
-		
 	move_and_slide()
  
